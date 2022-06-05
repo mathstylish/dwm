5b79fce55839eb6b6da44847a30aad11013ebddc
Matheus Henrique <matheus.hpp135@gmail.com>
2022-06-05 02:40:43 -0300

    change fonts

--- config.h
+++ config.h
@@ -17,7 +17,10 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "Hack Nerd Font:style=Regular:size=11:antialias=true:autohint=true" };
+static const char *fonts[]          = { 
+	"Hack Nerd Font:style=Regular:size=11:antialias=true:autohint=true",
+	"NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" 
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
